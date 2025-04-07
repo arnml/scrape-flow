b24be585f017309af2b0633282901c06f4f38ce4
Arnold Moya <dev.arn.ml@gmail.com>
2025-04-07 10:07:42 -0300

    feat: add static params generation for sign-in and sign-up pages

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,16 @@
 'use client';
 import { SignIn } from '@clerk/nextjs'
 
+// Add this function to enable static exports
+export function generateStaticParams() {
+  return [
+    { 'sign-in': [] },
+    { 'sign-in': ['start'] },
+    { 'sign-in': ['factor-one'] },
+    { 'sign-in': ['factor-two'] }
+  ]
+}
+
 export default function Page() {
   return <SignIn />
 }
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,16 @@
 'use client';
 import { SignUp } from '@clerk/nextjs'
 
+// Add this function to enable static exports
+export function generateStaticParams() {
+  return [
+    { 'sign-up': [] },
+    { 'sign-up': ['start'] },
+    { 'sign-up': ['factor-one'] },
+    { 'sign-up': ['factor-two'] }
+  ]
+}
+
 export default function Page() {
   return <SignUp />
 }
